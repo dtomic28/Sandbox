fa45d1567ae68b78631f162abf58dad76489b491
unknown <danijeltomic2004@gmail.com>
2020-03-01 17:29:54 +0100

    Nova naloga emso test

---
+++ emsoTest.py
@@ -0,0 +1,18 @@
+
+def preveriEmso():
+    vnos = input("Vnesite emso: ")
+    vsota = 0
+    faktor = 7
+    for i in range(0, len(emso)-1):
+        vsota += int(vnos[i])*faktor
+        faktor -= 1
+        if faktor==1:
+            faktor =7
+    zadnjaStevka = int(vnos[len(vnos)-1])
+    if (vsota + zadnjaStevka)%11==0:
+        return True
+    else:
+        return False:
+
+if __name__ == "__main__":
+    print(preveriEmso())

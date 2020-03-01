b35850d7234bd8568b369717edf5326464e6e2e9
unknown <danijeltomic2004@gmail.com>
2020-03-01 19:16:58 +0100

    Changed some errors

--- emsoTest.py
+++ emsoTest.py
@@ -3,7 +3,7 @@ def preveriEmso():
     vnos = input("Vnesite emso: ")
     vsota = 0
     faktor = 7
-    for i in range(0, len(emso)-1):
+    for i in range(0, len(vnos)-1):
         vsota += int(vnos[i])*faktor
         faktor -= 1
         if faktor==1:
@@ -12,7 +12,7 @@ def preveriEmso():
     if (vsota + zadnjaStevka)%11==0:
         return True
     else:
-        return False:
+        return False
 
 if __name__ == "__main__":
     print(preveriEmso())

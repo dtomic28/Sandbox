df0c700b8e15bc6ea438042e6d9076a75a3646d2
danimc55 <danijeltomic2004@gmail.com>
2022-04-04 23:53:13 +0200

    dadwad

--- Java/objectTelefon/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Random randomGenerator = new Random();
-        int randomInt = randomGenerator.nextInt(14);
-        System.out.print(randomInt);
-    }
-}

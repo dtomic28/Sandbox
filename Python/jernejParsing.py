0ff2a8607f2c02535f6d280277ea57f79413e318
Danijel Tomić <danijeltomic2004@gmail.com>
2022-09-03 14:03:42 +0200

    Jernej parse

---
+++ Python/jernejParsing.py
@@ -0,0 +1,506 @@
+"""
+data = [
+    {
+        "2": "virant.kosak@gmail.com ",
+        "3": "€ 37,04",
+        "4": "7",
+        "__ROW_NUMBER__": 2,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 1
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 11,24",
+        "4": "1",
+        "__ROW_NUMBER__": 3,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 2
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 185,43",
+        "4": "23",
+        "__ROW_NUMBER__": 4,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 3
+    },
+    {
+        "2": "janzekovic.damijan@gmail.com ",
+        "3": "€ 2.291,83",
+        "4": "213",
+        "__ROW_NUMBER__": 5,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 4
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 378,19",
+        "4": "20",
+        "__ROW_NUMBER__": 6,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 5
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "1",
+        "__ROW_NUMBER__": 7,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 6
+    },
+    {
+        "2": "golcer.ema@gmail.com ",
+        "3": "€ 51,34",
+        "4": "8",
+        "__ROW_NUMBER__": 8,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 7
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 44,73",
+        "4": "3",
+        "__ROW_NUMBER__": 9,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 8
+    },
+    {
+        "2": "janzekovic.damijan@gmail.com ",
+        "3": "€ 220,85",
+        "4": "22",
+        "__ROW_NUMBER__": 10,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 9
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "34",
+        "__ROW_NUMBER__": 11,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 10
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "22",
+        "__ROW_NUMBER__": 12,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 11
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "16",
+        "__ROW_NUMBER__": 13,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 12
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "71",
+        "__ROW_NUMBER__": 14,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 13
+    },
+    {
+        "2": "osterman.dejan@gmail.com ",
+        "3": "€ 1.320,92",
+        "4": "142",
+        "__ROW_NUMBER__": 15,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 14
+    },
+    {
+        "2": "lucija.cevnik@gmail.com ",
+        "3": "€ 463,23",
+        "4": "122",
+        "__ROW_NUMBER__": 16,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 15
+    },
+    {
+        "2": "lucija.cevnik@gmail.com ",
+        "3": "€ 1.089,83",
+        "4": "179",
+        "__ROW_NUMBER__": 17,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 16
+    },
+    {
+        "2": "manja.tancer@yahoo.com ",
+        "3": "€ 2,67",
+        "4": "1",
+        "__ROW_NUMBER__": 18,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 17
+    },
+    {
+        "2": "marija.m.bauman@gmail.com ",
+        "3": "€ 14,41",
+        "4": "2",
+        "__ROW_NUMBER__": 19,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 18
+    },
+    {
+        "2": "lpirc96@gmail.com ",
+        "3": "€ 151,30",
+        "4": "12",
+        "__ROW_NUMBER__": 20,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 19
+    },
+    {
+        "2": "barbara.popit13@gmail.com ",
+        "3": "€ 18,45",
+        "4": "3",
+        "__ROW_NUMBER__": 21,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 20
+    },
+    {
+        "2": "jasnam@outlook.com ",
+        "3": "€ 17,60",
+        "4": "2",
+        "__ROW_NUMBER__": 22,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 21
+    },
+    {
+        "2": "dejanstock@gmail.com ",
+        "3": "€ 7,10",
+        "4": "2",
+        "__ROW_NUMBER__": 23,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 22
+    },
+    {
+        "2": "dejanstock@gmail.com ",
+        "3": "€ 6,07",
+        "4": "1",
+        "__ROW_NUMBER__": 24,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 23
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "1",
+        "__ROW_NUMBER__": 25,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 24
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "1",
+        "__ROW_NUMBER__": 26,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 25
+    },
+    {
+        "2": "anarozman3@yahoo.com ",
+        "3": "€ 4,74",
+        "4": "1",
+        "__ROW_NUMBER__": 27,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 26
+    },
+    {
+        "2": "alenka.curin@guest.arnes.si ",
+        "3": "€ 45,73",
+        "4": "7",
+        "__ROW_NUMBER__": 28,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 27
+    },
+    {
+        "2": "zlatka.skok@gmail.com ",
+        "3": "€ 35,63",
+        "4": "5",
+        "__ROW_NUMBER__": 29,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 28
+    },
+    {
+        "2": "sabina.kosmrlkaucic@gmail.com ",
+        "3": "€ 308,89",
+        "4": "53",
+        "__ROW_NUMBER__": 30,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 29
+    },
+    {
+        "2": "dejanstock@gmail.com ",
+        "3": "€ 25,63",
+        "4": "4",
+        "__ROW_NUMBER__": 31,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 30
+    },
+    {
+        "2": "meta@metagroselj.com ",
+        "3": "€ 14,29",
+        "4": "3",
+        "__ROW_NUMBER__": 32,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 31
+    },
+    {
+        "2": "meta@metagroselj.com ",
+        "3": "€ 29,31",
+        "4": "3",
+        "__ROW_NUMBER__": 33,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 32
+    },
+    {
+        "2": "maya.peron@gmail.com ",
+        "3": "€ 6,02",
+        "4": "1",
+        "__ROW_NUMBER__": 34,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 33
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 103,22",
+        "4": "13",
+        "__ROW_NUMBER__": 35,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 34
+    },
+    {
+        "2": "petra.skarja@gmail.com",
+        "3": "€ 0,00",
+        "4": "29",
+        "__ROW_NUMBER__": 36,
+        "__SPREADSHEET_ID__": "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+        "__SHEET__": "Make_Pošiljanje",
+        "__IMTLENGTH__": 35,
+        "__IMTINDEX__": 35
+    }
+]
+
+
+unique = set()
+
+output = []
+keys = []
+
+for i in data:
+    keys = list(i.keys())
+    if i[keys[0]] not in unique:
+        unique.add(i[keys[0]])
+        tempDict = {keys[0]: i[keys[0]]} 
+        for j in keys[1:]:
+            tempDict[j] = [i[j]]
+        output.append(tempDict)
+    else:
+        for j in output:
+            if j[keys[0]] == i[keys[0]]:
+                for k in keys[1:]:
+                    j[k].append(i[k]) 
+                
+print(output)    
+
+"""
+
+data = {
+      "2":"petra.skarja@gmail.com",
+      "3":[
+         "€ 11,24",
+         "€ 185,43",
+         "€ 378,19",
+         "€ 0,00",
+         "€ 44,73",
+         "€ 0,00",
+         "€ 0,00",
+         "€ 0,00",
+         "€ 0,00",
+         "€ 0,00",
+         "€ 0,00",
+         "€ 103,22",
+         "€ 0,00"
+      ],
+      "4":[
+         "1",
+         "23",
+         "20",
+         "1",
+         "3",
+         "34",
+         "22",
+         "16",
+         "71",
+         "1",
+         "1",
+         "13",
+         "29"
+      ],
+      "__ROW_NUMBER__":[
+         3,
+         4,
+         6,
+         7,
+         9,
+         11,
+         12,
+         13,
+         14,
+         25,
+         26,
+         35,
+         36
+      ],
+      "__SPREADSHEET_ID__":[
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98",
+         "1RAOEb04uehSTHWrsJiElwk0hSMxgagKfdaAx7GG7m98"
+      ],
+      "__SHEET__":[
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje",
+         "Make_Pošiljanje"
+      ],
+      "__IMTLENGTH__":[
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35,
+         35
+      ],
+      "__IMTINDEX__":[
+         2,
+         3,
+         5,
+         6,
+         8,
+         10,
+         11,
+         12,
+         13,
+         24,
+         25,
+         34,
+         35
+      ]
+   }
+
+p = list(data.keys())
+sender = data[p[0]]
+myTable = "<table><tr>"
+
+
+for k in p[1:]:
+    for c,i in enumerate(data[k]):
+        myTable += "<td>%s</td>" %i
+        n = c+1;
+    myTable += "</tr><tr>"
+
+myTable += "</tr></table>"
+
+
+print(myTable)
+
+
+
+

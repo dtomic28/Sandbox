cf05c5a63c319b2f9450fe565e5503d0243a8fd1
Danijel Tomić <danijeltomic2004@gmail.com>
2022-11-15 11:57:03 +0100

    jerry

---
+++ Python/flask-test.py
@@ -0,0 +1,18 @@
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask
+
+def sensor():
+    """ Function for test purposes. """
+    print("Scheduler is alive!")
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(sensor,'interval',seconds=5)
+sched.start()
+
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.run()
